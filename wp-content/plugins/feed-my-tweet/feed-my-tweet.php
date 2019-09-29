890b3e551192af19835e4b0e37439340be7deb48
rajendramanandharjcub <rajendrakumar.manandhar@my.jcu.edu.au>
2019-09-30 09:51:03 +1000

    design added

--- wp-content/plugins/feed-my-tweet/feed-my-tweet.php
+++ wp-content/plugins/feed-my-tweet/feed-my-tweet.php
@@ -61,6 +61,8 @@ function register_my_cool_plugin_settings() {
 
 function my_cool_plugin_settings_page() {
 ?>
+
+
 <div class="wrap">
 <h1>Feed My Tweet</h1>
 
@@ -170,10 +172,19 @@ function twitter_feed_shortcode($atts)
         $slider_class = "";
     }
         ?>
-        <div class="container">
-            <div class="row">
-                <div class="twitter-wrap <?php echo $slider_class; ?>">
-                    <?php
+        
+
+        <!-- twitter example  -->
+        
+   <div class="container">
+      <div class="col-md-4">
+         <div class="panel panel-info">
+            <div class="panel-heading">
+               <h3 class="panel-title"><i class="fa fa-twitter-square" aria-hidden="true"></i>
+                  Tweet My Feed
+               </h3>
+            </div>
+            <?php
                     // Loop through them for output
                     foreach(json_decode($json) as $status) {
                      // Convert links back into actual links, otherwise they're just output as text
@@ -182,24 +193,55 @@ function twitter_feed_shortcode($atts)
                         $enhancedStatus = preg_replace('/https:\/\/t.co\/([a-zA-Z0-9]+)/i', '<a href="https://t.co/$1">http://$1</a>', $enhancedStatus);
                         // Finally, output a simple paragraph containing the tweet and a link back to the Twitter account itself. You can format/style this as you like.
                         ?>
-                        <div class="twitter-details">
-                            <div class="twitter-feed">
-                                <p>
-                                    &quot;<?php echo $enhancedStatus; ?>&quot;<br />
-                                    <a href="https://twitter.com/intent/user?screen_name=<?php echo $screen_name; ?>" target="_blank">@<?php echo $screen_name; ?></a>
-                                </p>
-                            </div>
-                            <div class="twitter-user">
-                                <p><?php echo $status->user->name; ?></p>
-                                <span>via Twitter</span>
-                                <img src="<?php echo $status->user->profile_image_url_https; ?>">
-                            </div>
-                        </div> <!-- twitter-details -->
-                        <?php
-                    } ?>
-                </div><!-- .twitter-slider -->
-            </div> <!-- .row -->
-        </div> <!-- .container -->
+            <div class="panel-body">
+            	            	
+            	<div class="card">
+            		<div class="card-body ">
+            			<div class="card-header">
+            				<img src="<?php echo $status->user->profile_image_url_https; ?>" class="img-circle card-img-left" style="-webkit-border-radius: initial; border-radius: 50%;"> 
+            				<span class="card-title">
+	            			<a class="twitter-timeline" data-width="100%" href="https://twitter.com/intent/user?screen_name=<?php echo $screen_name; ?>" target="_blank">Tweets by @<?php echo $screen_name; ?>  </a>
+	            		</span>
+            			</div>
+	            		
+	            		<div class="card-body text-center ">
+	            			<blockquote class="blockquote mb-0">
+	            			<p class="card-text">
+	            				<?php echo $enhancedStatus; ?>
+	            			</p>
+	            		</blockquote>
+	            			
+	            		</div>
+	            		<footer class="blockquote-footer">
+	            			via Twitter - <p><?php echo $status->user->name; ?>
+	            		</footer>
+	            		 
+            			
+            		</div>
+            		
+                
+</div>
+
+            </div>
+            <?php } ?>
+         </div>
+      </div>
+   </div>
+   <!-- <div class="col-md-4">
+   </div>
+</div> -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
         <?php
     $content = ob_get_clean();
     return $content;
